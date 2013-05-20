583c5c664e99f59ea2d36aa7681bb927efd3ae9b
Tony Findeisen <github@it-ony.de>
2013-05-20 18:05:39 +0200

    added EvalCheck

---
+++ test/EvalCheckTest.js
@@ -0,0 +1,25 @@
+var expect = require('chai').expect,
+    JsCop = require(".."),
+    Index = require("../lib"),
+    codeFromFunction = JsCop.Helper.codeFromFunction;
+
+describe("MissingSemicolonCheck", function () {
+
+    var cop;
+
+    before(function () {
+        cop = new JsCop();
+    });
+
+    it("should not validate call expressions without semicolon", function () {
+
+        var code = codeFromFunction(function () {
+            eval();
+        });
+
+        expect(cop.analyse(code).hasVialotions(Index.Checks.EvalCheck)).to.be.true;
+
+    });
+
+
+});
