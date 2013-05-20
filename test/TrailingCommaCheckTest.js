cf61d3e4aef43839df4509484b04bf59f7f9ffb7
Tony Findeisen <github@it-ony.de>
2013-05-20 17:57:47 +0200

    added TrailingCommaCheck

---
+++ test/TrailingCommaCheckTest.js
@@ -0,0 +1,40 @@
+var expect = require('chai').expect,
+    JsCop = require(".."),
+    Index = require("../lib"),
+    codeFromFunction = JsCop.Helper.codeFromFunction;
+
+describe("EmptyStatementCheck", function () {
+
+    var cop;
+
+    before(function () {
+        cop = new JsCop();
+    });
+
+    it("should detect trailing comma checks in arrays", function () {
+
+        var code = codeFromFunction(function () {
+            var x = ["foo",];
+        });
+
+        expect(cop.analyse(code).hasVialotions(Index.Checks.TrailingCommaCheck)).to.be.true;
+
+        code = codeFromFunction(function () {
+            var x = [,];
+        });
+
+        expect(cop.analyse(code).hasVialotions(Index.Checks.TrailingCommaCheck)).to.be.true;
+
+    });
+
+    it("should not detect trailing comma checks in arrays", function () {
+
+        var code = codeFromFunction(function () {
+            var x = ["foo"];
+        });
+
+        expect(cop.analyse(code).hasVialotions(Index.Checks.TrailingCommaCheck)).to.be.false;
+
+    });
+
+});
