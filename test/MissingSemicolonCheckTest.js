ab314f148c94f051281eb46809e6c92bd6ded302
Tony Findeisen <github@it-ony.de>
2013-05-25 11:01:58 +0200

    extended and fixed MissingSemicolon Check for variable statements

--- test/MissingSemicolonCheckTest.js
+++ test/MissingSemicolonCheckTest.js
@@ -31,6 +31,26 @@ describe("MissingSemicolonCheck", function () {
 
     });
 
+    it("should not validate variable statement without semicolon", function () {
+
+        var code = codeFromFunction(function () {
+            var x
+        });
+
+        expect(cop.analyse(code).hasViolation(Index.Checks.MissingSemicolonCheck)).to.be.true;
+
+    });
+
+    it("semicolon in var statement should be ok", function () {
+
+        var code = codeFromFunction(function () {
+            var x;
+        });
+
+        expect(cop.analyse(code).hasViolation(Index.Checks.MissingSemicolonCheck)).to.be.false;
+
+    });
+
 
     it("should not generate a warning for a call expressions with semicolon", function () {
 
@@ -41,5 +61,16 @@ describe("MissingSemicolonCheck", function () {
         expect(cop.analyse(code).hasViolation(Index.Checks.MissingSemicolonCheck)).to.be.false;
 
     });
+//
+//    it("should not generate a warning for combined statements", function () {
+//
+//        var code = codeFromFunction(function () {
+//            var x = foo(),
+//                y = 2;
+//        });
+//
+//        expect(cop.analyse(code).hasViolation(Index.Checks.MissingSemicolonCheck)).to.be.false;
+//
+//    });
 
 });
